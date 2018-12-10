30342cec35242456ccbf9fbef5354faf1e6781af
Brendan Mulvany <chaosyoshimage@gmail.com>
2018-12-10 22:29:13 +0000

    Completed assignment.

---
+++ VISR Acclerator Test/Assets/Scripts/Orbit.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour {
+
+ 
+    public Transform centerPoint;
+ 
+
+    void Start()
+    {
+    
+    }
+
+
+    void Update()
+    {
+
+        Rotate();
+
+    }
+
+    void Rotate()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.RotateAround(centerPoint.position, Vector3.up, 1);
+        }
+
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.RotateAround(centerPoint.position, Vector3.up, -1);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.RotateAround(centerPoint.position, Vector3.right, 1);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.RotateAround(centerPoint.position, Vector3.right, -1);
+        }
+
+    }
+}
